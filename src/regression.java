735b34902a90fa057c26f0a34dbe956aedc6db7a
Abhishek Dewan <ronald.abhishek@gmail.com>
2013-06-19 18:43:17 +0100

    Working Regression
    Regression may be working but it sheds light on the fact that the
    data-set may be incapable of handling the data.

--- src/regression.java
+++ src/regression.java
@@ -6,11 +6,11 @@ import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 public class regression {
 
 	public static void main(String [] args){
-		Date deadLine = new Date(2012,9,30,23,59,59);
-		Date deadLine1 = new Date(2012,9,29,21,41,59);
+		//Date deadLine = new Date(2012,9,30,23,59,59);
+		//Date deadLine1 = new Date(2012,9,29,21,41,59);
 		
-		System.out.println((deadLine.getTime()-deadLine1.getTime())/(1000*60*60));
-		/*OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+		//System.out.println((deadLine.getTime()-deadLine1.getTime())/(1000*60*60));
+		OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
 		double[] y = new double[]{11.0, 12.0, 13.0, 14.0, 15.0, 16.0};
 		double[][] x = new double[6][];
 		x[0] = new double[]{0, 0, 0, 0, 0};
@@ -23,8 +23,15 @@ public class regression {
 		
 		
 		double[] beta = regression.estimateRegressionParameters();
+		double[] residuals = regression.estimateResiduals();
 		for(int i =0;i<beta.length;i++){
-			System.out.format("The beta value for the %d parameter is %f\n",i,beta[i]);
-		}*/
+			System.out.println("Parameter "+i+" has regression coefficient value "+beta[i]);
+		}
+		
+		for(int i =0;i<residuals.length;i++){
+			System.out.println("Parameter "+i+" has error value "+residuals[0]);
+		}
+		
+		System.out.println(regression.estimateRegressionStandardError());
 	}
 }
--- src/start_learning.java
+++ src/start_learning.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
 
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
+
 /*Class that starts the learning process for the system.*/
 public class start_learning {
 
@@ -30,17 +32,23 @@ public class start_learning {
 	}
 	
 	public void execute_steps(){
-		multi_linear_regression();
+		multi_linear_regression_summrary_parameters();
 	}
 
-	private void multi_linear_regression() {
+	private void multi_linear_regression_summrary_parameters() {
+	  OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
 	  int imageCount = 0;
 	  int totalWordCount = 0;
 	  float submissionTimeLeft;
 	  int extendedDeadline;
 	  ResultSet resultSet = null;
 	  ResultSet tmpResultSet = null;
+	  String [] parameters = {"totalWordCount","imageCount","submissionTimeLeft"};
 	  int marks = 0;
+	  int counter=0;
+	  double[] marksDataSet = null;
+	  double[][] parametersDataSet = null;
+	  int totalRowsInDB = 0;
 	  String[] months = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
 	  
 	  String id;
@@ -50,6 +58,13 @@ public class start_learning {
 	 
 	  try {
 		resultSet = databaseStatement.executeQuery("select * from submissionTime");
+		tmpResultSet = databaseStatement1.executeQuery("select count(*) from imageDB");
+		while(tmpResultSet.next()){
+			totalRowsInDB = tmpResultSet.getInt(1);
+		}
+		marksDataSet = new double[totalRowsInDB+1];
+	    parametersDataSet = new double[totalRowsInDB+1][];
+	    
 		while(resultSet.next()){
 			id = resultSet.getString(1);
 		    int day = Integer.parseInt(resultSet.getString(2).substring(0,2));
@@ -66,7 +81,7 @@ public class start_learning {
 			int seconds = Integer.parseInt(resultSet.getString(3).substring(6,8));
 			
 			submittedDate = new Date(year,month,day,hrs,mins,seconds);
-			submissionTimeLeft = (deadLine.getTime() - submittedDate.getTime())/(1000*60*60); // value in hrs
+			submissionTimeLeft = (deadLine.getTime() - submittedDate.getTime())/(1000*60*60*24); // value in minutes
 			
 			if(resultSet.getString(5).contains("YES")){
 				extendedDeadline = 1;
@@ -85,8 +100,38 @@ public class start_learning {
 				marks = Integer.parseInt(tmpResultSet.getString(2));
 			}
 			
-			System.out.format("Id %s got %d marks for %d totalWordCount %d images and %f timeLeftForSubmission\n",id,marks,totalWordCount,imageCount,submissionTimeLeft);
+			//System.out.format("Id %s got %d marks for %d totalWordCount %d images and %f timeLeftForSubmission\n",id,marks,totalWordCount,imageCount,submissionTimeLeft);
+			marksDataSet[counter] = marks;
+			parametersDataSet[counter] = new double[]{totalWordCount,imageCount,submissionTimeLeft};
+			//parametersDataSet[counter] = new double[]{totalWordCount};
+			counter++;
 		}
+		
+	    marksDataSet[counter] =0;
+	    parametersDataSet[counter] = new double[]{0,0,0};
+		
+		regression.newSampleData(marksDataSet,parametersDataSet);
+	
+		double [] regressionCoefficients = regression.estimateRegressionParameters();
+		double [] errors = regression.estimateResiduals();
+		System.out.format("Regression Equation becomes :\n" +"marks obtained = %f + %f*%s + %f*%s + %f*%s\n",regressionCoefficients[0],regressionCoefficients[1],parameters[0],regressionCoefficients[2],parameters[1],regressionCoefficients[3],parameters[2]);
+		//System.out.format("Regression Equation becomes :\n" +"marks obtained = %f + %f*%s\n",regressionCoefficients[0],regressionCoefficients[1],parameters[0]);			  
+		
+		
+		System.out.println("		-------------");
+		System.out.println();
+		System.out.println("		|	Errors	|");
+		System.out.println(parameters[0]+"     |"+errors[0]+"|");
+		System.out.println(parameters[1]+"     |"+errors[1]+"|");
+		System.out.println(parameters[2]+"     |"+errors[2]+"|");
+		System.out.println("		-------------");
+		
+		System.out.println("Standard Error = "+ regression.estimateRegressionStandardError());
+		System.out.println("R-Square = "+regression.calculateRSquared());
+		
+		double[][] paramterVariance = regression.estimateRegressionParametersVariance();
+		
+		
 	} catch (SQLException e) {
 		
 		e.printStackTrace();
